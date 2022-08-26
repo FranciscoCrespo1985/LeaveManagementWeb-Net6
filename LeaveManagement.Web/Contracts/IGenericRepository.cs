70cd1678ccdd00997879c2a3fb3216020038d591
francisco.crespo <francisco.crespo@globant.com>
2022-08-26 20:08:39 -0300

    Added leave Type Repository End Of Repository Pattern Module

---
+++ LeaveManagement.Web/Contracts/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace LeaveManagement.Web.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetAsync(int? id);
+        Task<T> AddAsync(T entity);
+        Task<bool> Exist(int id); 
+        Task DeleteAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task UpdateAsync(T entity);  
+           
+    }
+}
---
+++ LeaveManagement.Web/Repositories/LeaveTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using LeaveManagement.Web.Contracts;
+using LeaveManagement.Web.Data;
+
+namespace LeaveManagement.Web.Repositories
+{
+    public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeReposity
+    {
+        public LeaveTypeRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
